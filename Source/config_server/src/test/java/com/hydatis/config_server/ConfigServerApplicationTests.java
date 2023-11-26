a9ce4fd8b7c8f9cd009f1028530993703164d87a
manarLABIDI <manarabidi4@gmail.com>
2023-11-26 23:09:51 +0100

    config server

--- Source/config_server/src/test/java/com/hydatis/config_server/ConfigServerApplicationTests.java
+++ Source/config_server/src/test/java/com/hydatis/config_server/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hydatis.Configserver;
+package com.hydatis.config_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ConfigServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
